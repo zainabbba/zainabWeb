7d44e9de0218585c6365299ede5db46381fb3b24
zainabbba <zainab.alshingiti@gmail.com>
2022-10-03 13:54:11 +0300

    first commit

---
+++ app.js
@@ -0,0 +1,11 @@
+let sidemenu=document.getElementsByClassName('sidemenu')[0]
+
+
+function openmenu(){
+    sidemenu.style.right='0'
+    console.log('hello');
+}
+
+function closemenu(){
+    sidemenu.style.right='-200px'
+}
