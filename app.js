cba14ca8285aa0ef58383345bd2cbf8f84899e15
zainabbba <zainab.alshingiti@gmail.com>
2022-10-25 13:09:52 +0300

    edit new file

--- app.js
+++ app.js
@@ -9,3 +9,52 @@ function openmenu(){
 function closemenu(){
     sidemenu.style.right='-200px'
 }
+
+
+
+let menu = document.querySelectorAll("header nav a");
+
+
+
+function reveal() {
+  var reveals = document.querySelectorAll(".reveal");
+
+  for (var i = 0; i < reveals.length; i++) {
+    var windowHeight = window.innerHeight;
+    var elementTop = reveals[i].getBoundingClientRect().top;
+    var elementVisible = 150;
+
+    if (elementTop < windowHeight - elementVisible) {
+      reveals[i].classList.add("active");
+    } else {
+      reveals[i].classList.remove("active");
+    }
+  }
+}
+
+function revealheading() {
+    let reavealheading=document.querySelector('.reavealheading')
+  
+
+      var windowHeight = window.innerHeight;
+      var elementTop = reavealheading.getBoundingClientRect().top;
+      var elementVisible =150;
+  
+      if (elementTop < windowHeight - elementVisible) {
+        reavealheading.classList.add("active");
+      } else {
+        reavealheading.classList.remove("active");
+      }
+    }
+  
+window.addEventListener("scroll", reveal);
+window.addEventListener("scroll", revealheading);
+
+// To check the scroll position on page load
+reveal();
+revealheading();
+
+
+
+
+
